92f3561f401c122a81d920250be840ce896ea0fe
Colin Keleher <cbkeleher@gmail.com>
2026-03-25 23:11:17 -0500

    Tests with intentional failure

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -25,5 +25,36 @@ public class Addition
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add("1", null));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, "1"));
 		Assert.ThrowsException<ArgumentNullException>(() => Program.Add(null, null));
-	}
+	}	
+}
+
+[TestClass]
+public class SubtractionTests
+{
+	[TestMethod]
+	public void Subtract_Valid_Keleher() =>  Assert.AreEqual(5, Program.Subtract("10", "5"));
+}
+
+[TestClass]
+public class MultiplicationTests
+{
+	[TestMethod]
+	public void Multiply_Valid_Keleher() => Assert.AreEqual(50, Program.Multiply("10", "5"));
+}
+
+[TestClass]
+public class DivisionTests
+{
+	[TestMethod]
+	public void Divide_Valid_Keleher() => Assert.AreEqual(2, Program.Divide("10", "5"));
+}
+
+[TestClass]
+public class PowerTests
+{
+	[TestMethod]
+	public void Power_Valid_Keleher() => Assert.AreEqual(100, Program.Power("10", "2"));
+
+	[TestMethod]
+	public void Power_Invalid_Keleher() => Assert.AreEqual(9, Program.Power("2", "3"));
 }
